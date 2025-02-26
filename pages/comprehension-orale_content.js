53f77bc6959ceaf8ba0d1e5b406f54020d63e10d
Joan <joan.vigne@hotmail.fr>
2025-02-26 10:11:54 +0000

    more audios

--- pages/comprehension-orale_content.js
+++ pages/comprehension-orale_content.js
@@ -43,6 +43,16 @@ const audios = [
     level: "A1",
     items: [
       {
+        number: 4,
+        linkAudio: "../audios/jemappellemarieetjhabiteaparis.mp3",
+        answer: "Je m’appelle Marie et j’habite à Paris.",
+      },
+    ],
+  },
+  {
+    level: "A1",
+    items: [
+      {
         number: 1,
         linkAudio: "../audios/quelageas-tu.mp3",
         answer: "quel âge as-tu ?",
@@ -61,7 +71,7 @@ const audios = [
     ],
   },
   {
-    level: "A2",
+    level: "A1",
     items: [
       {
         number: 3,
@@ -71,7 +81,7 @@ const audios = [
     ],
   },
   {
-    level: "A2",
+    level: "A1",
     items: [
       {
         number: 4,
@@ -81,7 +91,7 @@ const audios = [
     ],
   },
   {
-    level: "A2",
+    level: "A1",
     items: [
       {
         number: 1,
@@ -91,7 +101,7 @@ const audios = [
     ],
   },
   {
-    level: "A2",
+    level: "A1",
     items: [
       {
         number: 2,
@@ -111,12 +121,45 @@ const audios = [
     ],
   },
   {
+    level: "A1",
+    items: [
+      {
+        number: 3,
+        linkAudio: "../audios/ledrapeaufrancaisestbleublancetrouge.mp3",
+        answer: "Le drapeau français est bleu, blanc et rouge.",
+      },
+    ],
+  },
+  {
     level: "A2",
     items: [
       {
         number: 4,
         linkAudio: "../audios/ilsontpartisenvacances.mp3",
         answer: "ils sont partis en vacances.",
+      },
+    ],
+  },
+  {
+    level: "A2",
+    items: [
+      {
+        number: 4,
+        linkAudio:
+          "../audios/enfranceonmangesouventducroissantaupetitdejeuner.mp3",
+        answer: " En France, on mange souvent du croissant au petit-déjeuner.",
+      },
+    ],
+  },
+  {
+    level: "A2",
+    items: [
+      {
+        number: 3,
+        linkAudio:
+          "../audios/leweekenddernierjesuisalleaumarchepouracheterdesfruitsetdeslegumes.mp3",
+        answer:
+          "Le week-end dernier, je suis allé au marché pour acheter des fruits et des légumes.",
       },
     ],
   },
@@ -133,6 +176,52 @@ const audios = [
     ],
   },
   {
+    level: "A2",
+    items: [
+      {
+        number: 1,
+        linkAudio:
+          "../audios/enhiveronpeutfaireduskidanslesalpesfrancaises.mp3",
+        answer: "En hiver, on peut faire du ski dans les Alpes françaises.",
+      },
+    ],
+  },
+  {
+    level: "B1",
+    items: [
+      {
+        number: 1,
+        linkAudio:
+          "../audios/lelouvreestungrandmuseeoulonpeutvoirlajoconde.mp3",
+        answer: "Le Louvre est un grand musée où l’on peut voir la Joconde.",
+      },
+    ],
+  },
+  {
+    level: "B1",
+    items: [
+      {
+        number: 1,
+        linkAudio:
+          "../audios/apprendreunelanguedemandedutempsetbeaucoupdepratique.mp3",
+        answer:
+          "Apprendre une langue demande du temps et beaucoup de pratique.",
+      },
+    ],
+  },
+  {
+    level: "B1",
+    items: [
+      {
+        number: 1,
+        linkAudio:
+          "../audios/levinetlefromagefontpartiedesspecialiteslespluscelebresdelafrance.mp3",
+        answer:
+          "Le vin et le fromage font partie des spécialités les plus célèbres de la France.",
+      },
+    ],
+  },
+  {
     level: "B1",
     items: [
       {
@@ -141,6 +230,144 @@ const audios = [
         answer:
           "Bonjour monsieur, bonjour, que désirez-vous? une baguette et un pain au chocolat s'il vous plait. Pain au chocolat. Et avec ceci? Je vais prendre deux croissants au beurre. D'accord. Bon voilà monsieur, c'est tout? C'est tout, je vous dois combien? Au total, ça fait six euros. Carte ou espèce? Carte. Votre code s'il vous plait. Oui. Merci et bonne journée. À vous aussi, au revoir.",
         indice: `Attention, ignorez les sons d'hésitation comme "euh". Les nombres doivent être écrits en lettres.`,
+      },
+    ],
+  },
+  {
+    level: "B2",
+    items: [
+      {
+        number: 1,
+        linkAudio:
+          "../audios/lequartierdupantheonaparisestconnupoursesuniversitesetsonheritageintellectuel.mp3",
+        answer:
+          "Le quartier du Panthéon à Paris est connu pour ses universités et son héritage intellectuel.",
+      },
+    ],
+  },
+  {
+    level: "B2",
+    items: [
+      {
+        number: 1,
+        linkAudio:
+          "../audios/lefromageroquefortestaffinedansdescavesnaturellescequiluidonnesongoutunique.mp3",
+        answer:
+          "Le fromage Roquefort est affiné dans des caves naturelles, ce qui lui donne son goût unique.",
+      },
+    ],
+  },
+  {
+    level: "B2",
+    items: [
+      {
+        number: 1,
+        linkAudio:
+          "../audios/lemontblancplushautsommetdeuropeoccidentaleattirechaqueanneedenombreuxalpinistes.mp3",
+        answer:
+          "Le Mont-Blanc, plus haut sommet d’Europe occidentale, attire chaque année de nombreux alpinistes.",
+      },
+    ],
+  },
+  {
+    level: "B2",
+    items: [
+      {
+        number: 1,
+        linkAudio:
+          "../audios/lafetedelamusiquepermetauxartistesamateursetprofessionnelsdejouerdanslesrues.mp3",
+        answer:
+          "  La fête de la musique permet aux artistes amateurs et professionnels de jouer dans les rues.",
+      },
+    ],
+  },
+  {
+    level: "B2",
+    items: [
+      {
+        number: 1,
+        linkAudio:
+          "../audios/lavilledelyonestcelebrepoursafetedeslumieresetsagastronomie.mp3",
+        answer:
+          "La ville de Lyon est célèbre pour sa fête des Lumières et sa gastronomie.",
+      },
+    ],
+  },
+  {
+    level: "B2",
+    items: [
+      {
+        number: 1,
+        linkAudio:
+          "../audios/lemontsaintmichelestunlieuhistoriquequiattiredenombreuxtouristes.mp3",
+        answer:
+          "Le Mont-Saint-Michel est un lieu historique qui attire de nombreux touristes.",
+      },
+    ],
+  },
+  {
+    level: "C1",
+    items: [
+      {
+        number: 1,
+        linkAudio:
+          "../audios/comprendreunecultureetrangerenecessiteuneouverturedespritetunecertainecuriosite.mp3",
+        answer: `Comprendre une culture étrangère nécessite une ouverture d’esprit et une certaine curiosité.`,
+      },
+    ],
+  },
+  {
+    level: "C1",
+    items: [
+      {
+        number: 1,
+        linkAudio:
+          "../audios/lesystemedesgrandsecolesenfranceestunespecificitedupaysageeducatiffrancaisformantlelitedansdiversdomaines.mp3",
+        answer: `Le système des Grandes Écoles en France est une spécificité du paysage éducatif français, formant l’élite dans divers domaines.`,
+      },
+    ],
+  },
+  {
+    level: "C1",
+    items: [
+      {
+        number: 1,
+        linkAudio:
+          "../audios/lechateaudeversaillesestconnupoursesjardinsmagnifiquesetsagaleriedesglaces.mp3",
+        answer: `Le château de Versailles est connu pour ses jardins magnifiques et sa galerie des Glaces.`,
+      },
+    ],
+  },
+  {
+    level: "C2",
+    items: [
+      {
+        number: 1,
+        linkAudio:
+          "../audios/linfluencedelaphilosophiedeslumieresurlesvaleursrepublicainesfrancaisestindeniable.mp3",
+        answer: `L’influence de la philosophie des Lumières sur les valeurs républicaines françaises est indéniable.`,
+      },
+    ],
+  },
+  {
+    level: "C2",
+    items: [
+      {
+        number: 1,
+        linkAudio:
+          "../audios/loeuvredemarcelproustnotammentalarecherchedutempsperduestunpilierdelalitteraturefrancaise.mp3",
+        answer: ` L’œuvre de Marcel Proust, notamment À la recherche du temps perdu, est un pilier de la littérature française.`,
+      },
+    ],
+  },
+  {
+    level: "C2",
+    items: [
+      {
+        number: 1,
+        linkAudio:
+          "../audios/larchitecturegothiqueillustreeparlacathedraleNotredamedeparistemoignedunsavoirfaireexceptionneldumoyenage.mp3",
+        answer: `L’architecture gothique, illustrée par la cathédrale Notre-Dame de Paris, témoigne d’un savoir-faire exceptionnel du Moyen Âge.`,
       },
     ],
   },
